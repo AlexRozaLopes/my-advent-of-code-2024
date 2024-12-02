3131335f99cd32d8f98e64cdb0fd4bf5d0fd1207
AlexRozaLopes <alexrozaestudos@gmail.com>
2024-12-02 08:44:31 -0300

    desafio do dia 1 finalizado!

--- src/day/day_one.rs
+++ src/day/day_one.rs
@@ -1,6 +1,33 @@
 use std::ops::Sub;
 use std::str::Lines;
 
+pub fn distance_between(input: &str) -> i32 {
+    let (list_left, list_right) = split_in_list(input.lines());
+    let mut list_sum = Vec::<i32>::new();
+
+    for i in 0..list_left.len() {
+        let number = list_left[i].sub(list_right[i]).abs();
+        list_sum.push(number);
+    }
+    list_sum.iter().sum()
+}
+
+fn split_in_list(lines: Lines) -> (Vec<i32>, Vec<i32>) {
+    let mut list_left: Vec<i32> = Vec::new();
+    let mut list_right: Vec<i32> = Vec::new();
+    lines.for_each(|line| {
+        let split_values = line
+            .split_whitespace()
+            .map(|s| s.parse::<i32>().unwrap())
+            .collect::<Vec<i32>>();
+        list_left.push(split_values[0]);
+        list_right.push(split_values[1]);
+    });
+    list_left.sort();
+    list_right.sort();
+    (list_left, list_right)
+}
+
 #[test]
 fn test_distance_between() {
     let input = "3   4
@@ -13,29 +40,23 @@ fn test_distance_between() {
     assert_eq!(11, distance_between(input))
 }
 
-pub fn distance_between(input: &str) -> i32 {
-    let (list_left, list_right) = split_in_list(input.lines());
-    let mut list_sum = Vec::<i32>::new();
-
-    for i in 0..list_left.len() {
-        let number = list_left[i].sub(list_right[i]).abs();
-        println!("{}", number);
-        list_sum.push(number);
-    }
-    list_sum.iter().sum()
+#[test]
+fn test_similarity_score() {
+    let input = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
+    assert_eq!(31, similarity_score(input))
 }
 
-fn split_in_list(lines: Lines) -> (Vec<i32>, Vec<i32>) {
-    let mut list_left: Vec<i32> = Vec::new();
-    let mut list_right: Vec<i32> = Vec::new();
-    lines.for_each(|line| {
-        let split_values = line.split_whitespace().map(|s| s.parse::<i32>().unwrap()).collect::<Vec<i32>>();
-        list_left.push(split_values[0]);
-        list_right.push(split_values[1]);
+pub fn similarity_score(input: &str) -> i32 {
+    let lists = split_in_list(input.lines());
+    let mut score = 0;
+    lists.0.iter().for_each(|n| {
+        let count = lists.1.iter().filter(|e| *e == n).count() as i32;
+        score = n * count + score;
     });
-    list_left.sort();
-    list_right.sort();
-    println!("{:?}", list_left);
-    println!("{:?}", list_right);
-    (list_left, list_right)
+    score
 }
--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,11 @@ mod day;
 
 fn main() {
     println!(
-        "{}",
-        format!(
-            "DAY ONE: {}",
-            day_one::distance_between(include_str!("data/day-one.txt"))
-        )
+        "Day 1: Historian Hysteria: PART ONE - ANSWER {}",
+        day_one::distance_between(include_str!("data/day-one.txt"))
+    );
+    println!(
+        "Day 1: Historian Hysteria: PART TWO - ANSWER {}",
+        day_one::similarity_score(include_str!("data/day-one.txt"))
     )
 }
