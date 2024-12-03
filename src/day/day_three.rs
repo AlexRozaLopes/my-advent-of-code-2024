f91b572ed917465100c0bf9a96a568e51a31d24d
AlexRozaLopes <alexrozaestudos@gmail.com>
2024-12-03 18:53:32 -0300

    desafio do dia 2 parte 2 finalizado!

---
+++ src/day/day_three.rs
@@ -0,0 +1,23 @@
+use regex::Regex;
+
+pub fn process(input: &str) -> String {
+    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+
+    let result = re
+        .captures_iter(input)
+        .map(|caps| {
+            let first = caps.get(1).unwrap().as_str().parse::<u32>().unwrap();
+            let second = caps.get(2).unwrap().as_str().parse::<u32>().unwrap();
+            first * second
+        })
+        .sum::<u32>();
+
+    result.to_string()
+}
+
+#[test]
+fn test_process() {
+    let input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+
+    assert_eq!(process(input), "161");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use crate::day::{day_one, day_two};
+use crate::day::{day_one, day_two,day_three};
 
 mod day;
 
@@ -18,5 +18,9 @@ fn main() {
     println!(
         "Day 2: Red-Nosed Reports: PART TWO - ANSWER {}",
         day_two::reports_is_safe_with_one_erro(include_str!("data/day-two.txt"))
+    );
+    println!(
+        "Day 3: Mull It Over: PART ONE - ANSWER {}",
+        day_three::process(include_str!("data/day-three.txt"))
     )
 }
